9aced4205b6fae6119f5f6234e280136f5a05a82
gziltoid <g.ziltoid@gmail.com>
2022-01-29 00:02:30 +0100

    Get all paths permutations and each path length

--- postman/postman.py
+++ postman/postman.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
-
-
 from collections import namedtuple
+from itertools import permutations
 
-
-points = ((0, 2), (2, 5), (5, 2), (6, 6), (8, 3))
-PATHS_NUMBER = 24  # (n - 1)! -> (5 - 1)! = 4! = 24
 
 Point = namedtuple('Point', 'x y')
+points = (Point(0, 2), Point(2, 5), Point(5, 2), Point(6, 6), Point(8, 3))
+PATHS_NUMBER = 24  # (n - 1)! -> (5 - 1)! = 4! = 24
 
 
 def get_distance(p1, p2):
     return ((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2) ** 0.5
+
+
+def get_full_path_distance(path):
+    return get_distance(points[0], points[path[0]]) + \
+        get_distance(points[path[0]], points[path[1]]) + \
+        get_distance(points[path[1]], points[path[2]]) + \
+        get_distance(points[path[2]], points[path[3]]) + \
+        get_distance(points[path[3]], points[0])
+
+
+paths = [p for p in permutations(range(1, 5)) if p[0] <= p[-1]]
+distances = [get_full_path_distance(path) for path in paths]
+print(min(distances))
