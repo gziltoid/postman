233f28361f173758acfea076572ee1aeb23f4dc5
gziltoid <g.ziltoid@gmail.com>
2022-01-29 01:30:25 +0100

    Refactoring

--- postman/postman.py
+++ postman/postman.py
@@ -4,22 +4,24 @@ from itertools import permutations
 
 
 Point = namedtuple('Point', 'x y')
-points = (Point(0, 2), Point(2, 5), Point(5, 2), Point(6, 6), Point(8, 3))
-PATHS_NUMBER = 24  # (n - 1)! -> (5 - 1)! = 4! = 24
 
 
 def get_distance(p1, p2):
     return ((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2) ** 0.5
 
 
-def get_full_path_distance(path):
-    return get_distance(points[0], points[path[0]]) + \
-        get_distance(points[path[0]], points[path[1]]) + \
-        get_distance(points[path[1]], points[path[2]]) + \
-        get_distance(points[path[2]], points[path[3]]) + \
-        get_distance(points[path[3]], points[0])
+def get_path_length(points, path):
+    full_path = [0, *path, 0]
+    total_length = 0
+    for p1, p2 in zip(full_path, full_path[1:]):
+        total_length += get_distance(points[p1], points[p2])
+    return total_length
 
 
-paths = [p for p in permutations(range(1, 5)) if p[0] <= p[-1]]
-distances = [get_full_path_distance(path) for path in paths]
-print(min(distances))
+if __name__ == '__main__':
+    points = (Point(0, 2), Point(2, 5), Point(5, 2), Point(6, 6), Point(8, 3))
+
+    paths = (p for p in permutations(range(1, 5)) if p[0] <= p[-1])
+    path_lengths = (get_path_length(points, path) for path in paths)
+    min_path_length = min(path_lengths)
+    print(min_path_length)
