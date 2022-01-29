7661d89cad43dfb1551c0dcb6d2d17ff26ce991a
gziltoid <g.ziltoid@gmail.com>
2022-01-29 02:46:46 +0100

    Add result formatting

--- postman/postman.py
+++ postman/postman.py
@@ -18,10 +18,23 @@ def get_path_length(points, path):
     return total_length
 
 
+def format_result(points, path, path_length):
+    full_path = [0, *path, 0]
+    s = f'({points[0].x}, {points[0].y})'
+    total_length = 0
+    for p1, p2 in zip(full_path, full_path[1:]):
+        total_length += get_distance(points[p1], points[p2])
+        s += f' -> ({points[p2].x}, {points[p2].y})[{total_length}]'
+    s += f' = {path_length}'
+    return s
+
+
 if __name__ == '__main__':
     points = (Point(0, 2), Point(2, 5), Point(5, 2), Point(6, 6), Point(8, 3))
 
-    paths = (p for p in permutations(range(1, 5)) if p[0] <= p[-1])
-    path_lengths = (get_path_length(points, path) for path in paths)
-    min_path_length = min(path_lengths)
-    print(min_path_length)
+    paths = [p for p in permutations(range(1, 5)) if p[0] <= p[-1]]
+    paths_lengths = [get_path_length(points, path) for path in paths]
+    min_path_length = min(paths_lengths)
+    min_path_index = paths_lengths.index(min_path_length)
+    min_path = paths[min_path_index]
+    print(format_result(points, min_path, min_path_length))
